96c92a686531a69d6470679c7038631b3efad3b4
zhouzp16 <zhouzp16@163.com>
2020-12-14 21:08:13 +0800

    update

--- src/LPANNI/LPANNI.cpp
+++ src/LPANNI/LPANNI.cpp
@@ -12,41 +12,22 @@ int main()
 	LPANNI worker = LPANNI();
 	unsigned int alpha = 3;
 	unsigned int T = 100;
-	unsigned int repeat = 10;
-	float total_time = 0;
+	unsigned int repeat = 3;
 
-	//for (unsigned int i = 0; i < repeat; i++) {
-	//	//Graph graph = Graph(string("../../data/sample.txt"));
-	//	Graph graph = Graph(string("../../data/amazon/amazon.ungraph.txt"));
-	//	
-	//	clock_t start_time = clock();
-	//	worker.calculate_NI(graph);
-	//	// graph.printni();
-
-	//	worker.calculate_SIM_NNI(graph, alpha);
-	//	// graph.printsim();
-	//	// graph.printnni();
-
-	//	worker.propagation(graph, T);
-	//	// graph.printcommunitypernode();
-
-	//	clock_t end_time = clock();
-	//	float t = (end_time - start_time) / (double)CLOCKS_PER_SEC;
-	//	total_time += t;
-
-	//	/*char save_name[50];
-	//	sprintf_s(save_name, 50, "../../result/amazon_%d_%d.txt", alpha, t);
-	//	file *fp;
-	//	fopen_s(&fp, save_name, "w");
-	//	char time[50];
-	//	unsigned int offset = sprintf_s(time, 50, "%.3f\n", t);
-	//	fwrite(time, offset, 1, fp);
-	//	fclose(fp);
-	//	graph.savecommunity(string(save_name));*/
-
-	//	cout << "time: " << t << endl;
-	//}
-	//return 0;
+	// Amazon
+	// Graph graph = Graph(string("../../data/sample.txt"));
+	Graph graph = Graph(string("../../data/amazon/amazon.ungraph.txt"));
+	clock_t start_time = clock();
+	worker.calculate_NI(graph);
+	// graph.printni();
+	worker.calculate_SIM_NNI(graph, alpha);
+	// graph.printsim();
+	// graph.printnni();
+	worker.propagation(graph, T);
+	// graph.printcommunitypernode();
+	char save_name[50];
+	sprintf_s(save_name, 50, "../../result/amazon_%d_%d.txt", alpha, t);
+	graph.saveCommunity(string(save_name));
 
     // LFR benchmark
 	/*vector<unsigned int> Ns = { 1000,5000,10000 };
@@ -67,26 +48,25 @@ int main()
 	}*/
 
 	// LRF time complexity
-	vector<unsigned int> Ns = { 1000,2000,3000,4000,5000,6000,7000,8000,9000,10000 };
+	/*vector<unsigned int> Ns = { 10000,20000,30000,40000,50000,60000,70000,80000,90000,100000 };
 	for (unsigned int N : Ns) {
 		float total_time = 0;
 		for (unsigned int i = 0; i < repeat; i++) {
 			char save_name[50];
-			sprintf_s(save_name, 50, "../../time complexity/LFR/N-%d-mu0.1-om3", N);
+			sprintf_s(save_name, 50, "../../data/time complexity/N-%d-mu0.1-om3", N);
+			
 			Graph graph = Graph(string(save_name) + string("/network.dat"));
-
+			
 			clock_t start_time = clock();
-
 			worker.calculate_NI(graph);
 			worker.calculate_SIM_NNI(graph, alpha);
 			worker.propagation(graph, T);
-
 			clock_t end_time = clock();
 			float t = (end_time - start_time) / (double)CLOCKS_PER_SEC;
 			total_time += t;
 		}
 		cout << "N=" << N << ", time: " << total_time / repeat << endl;
-	}
+	}*/
 	
 	return 0;
 }
