481b5c09f84ffca3deb46ff7b870dd4f50fb8448
zhouzp16 <zhouzp16@163.com>
2020-12-14 20:51:50 +0800

    correct data

--- src/LFR/generate.py
+++ src/LFR/generate.py
@@ -3,13 +3,13 @@ import re
 import shutil
 
 if __name__ == "__main__":
-    dirname = 'exp'
-    for N in [1000, 5000, 10000]:
+    dirname = 'LFR'
+    for N,k,maxk,minc,maxc,on in [(1000,10,50,10,50,100), (5000,10,50,20,100,500), (10000,20,100,20,100,2000)]:
         for mu in [0.1, 0.3]:
             for om in range(2,9):
                 if not os.path.exists('{}/N-{}-mu{:1.1f}-om{}'.format(dirname, N, mu, om)):
                     os.makedirs('{}/N-{}-mu{:1.1f}-om{}'.format(dirname, N, mu, om))
-                s = os.popen('.\\benchmark -k 10 -maxk 50 -mu {} -minc 10 -maxc 50 -om {} -N {} -on {}'.format(mu, om, N, N//10)).readlines()
+                s = os.popen('.\\benchmark -k {} -maxk {} -mu {} -minc {} -maxc {} -om {} -N {} -on {}'.format(k, maxk, mu, minc, maxc, om, N, on)).readlines()
                 ans = re.match('network\sof\s(\d*)\svertices\sand\s(\d*)\sedges', s[-6])
                 node_num = int(ans[1])
                 edge_num = int(ans[2])
