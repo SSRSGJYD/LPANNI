c9f4795d1464f2ca5b11da0432bc86c9ad51a8e7
SiasDoming <1130063240@qq.com>
2020-12-18 15:17:27 +0800

    update FScore

--- src/Metrics/FScore.py
+++ src/Metrics/FScore.py
@@ -5,6 +5,7 @@
 
 
 import time
+import json
 
 
 # In[2]:
@@ -71,15 +72,22 @@ if __name__ == '__main__':
     #communityFilePath = "../../data/Amazon/amazon.top5000.cmty.txt"
     #resultFilePath = "../../result/amazon_result.txt"
     #fScore, precision, recall = calculateFScore(communityFilePath, resultFilePath)
-    with open('FScore.txt', 'w') as fileHandle:
+    with open('FScore.json', 'w') as fileHandle:
         #fileHandle.write('Amazon:\n\tFScore: %f\n\tPrecision: %f\n\tRecall: %f\n' % (fScore, precision, recall))
+        jsonObj = {}
         for i in [1000, 5000, 10000]:
+            jsonObj[str(i)] = {}
             for j in [1, 3]:
+                jsonObj[str(i)]['0.%d' % j] = {'FScore': [], 'Precision': [], 'Recall': []}
                 for k in [2, 3, 4, 5, 6, 7, 8]:
                     resultFilePath = "../../data/LFR/N-%d-mu0.%d-om%d/result.dat" % (i, j, k)
                     communityFilePath = "../../data/LFR/N-%d-mu0.%d-om%d/community.dat" % (i, j, k)
                     fScore, precision, recall = calculateFScore(communityFilePath, resultFilePath)
-                    fileHandle.write('N-%d-mu0.%d-om%d:\n\tFScore: %f\n\tPrecision: %f\n\tRecall: %f\n' % (i, j, k, fScore, precision, recall))
+                    #fileHandle.write('N-%d-mu0.%d-om%d:\n\tFScore: %f\n\tPrecision: %f\n\tRecall: %f\n' % (i, j, k, fScore, precision, recall))
+                    jsonObj[str(i)]['0.%d' % j]['FScore'].append(fScore)
+                    jsonObj[str(i)]['0.%d' % j]['Precision'].append(precision)
+                    jsonObj[str(i)]['0.%d' % j]['Recall'].append(recall)
+        fileHandle.write(json.dumps(jsonObj))
     completeTime = time.time()
     print('Result Written in FScore.txt')
     print('Running Time: %.3fs' % (completeTime-startTime))
